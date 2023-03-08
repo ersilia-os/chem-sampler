ab3748fe03b4b32bec8f6d0d180fd9eb976c5919
anamika-yadav99 <anamika310.yada@gmail.com>
2023-03-08 10:05:54 +0530

    readme updated

--- chemsampler/samplers/bimodal/sampler.py
+++ chemsampler/samplers/bimodal/sampler.py
@@ -12,13 +12,12 @@ class BimodalSampler:
             smiles_list = SimilaritySearcher(self.fp_filename).read_db_smiles()
             SimilaritySearcher(self.fp_filename).fit(smiles_list)
 
-    def sample(self, n , smiles_list = [],  search_pre_calculated=False, cutoff = 0.7):
+    def sample(self, n , smiles_list = [],  search_pre_calculated=False, cutoff = 0.6):
         if search_pre_calculated==True:
             samples = []
             for smile in smiles_list:
                 _samples = SimilaritySearcher(self.fp_filename).search(smile, cutoff)
                 if len(_samples) == 0:
-                    print('Coudn\'t find samples in the cutoff range : {}. Generating random samples:'.format(cutoff))
                     _samples = RandomSearcher(self.db_smiles_filename).search(n)
                 samples += _samples 
         
