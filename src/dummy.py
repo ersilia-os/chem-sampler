d9d142ea3249782750e8a852f801ce479f327798
anamika-yadav99 <anamika310.yada@gmail.com>
2023-03-21 17:34:52 +0530

    cleaning repo

--- src/dummy.py
+++
@@ -1,7 +0,0 @@
-from chemsampler import ChemSampler
-from chemsampler import example
-
-smiles_list = example()
-sampler = ChemSampler()
-sampled_smiles = sampler.sample(smiles_list, Sampler= 'ChemblSampler' , num_samples=100, sim_ub=0.95, sim_lb=0.3, distribution="ramp")
-print(sampled_smiles)
